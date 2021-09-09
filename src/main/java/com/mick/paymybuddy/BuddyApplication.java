66b51355f5d17af66226f506147b6f51bee07894
mickael j <joseph.mickk@gmail.com>
2021-09-09 13:31:49 +0200

    first

---
+++ src/main/java/com/mick/paymybuddy/BuddyApplication.java
@@ -0,0 +1,13 @@
+package com.mick.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyApplication.class, args);
+	}
+
+}
